f5197e1aec0157df55f50033f76008a42d5c3789
Sheik Rasheed <sheikrasheed@gmail.com>
2024-02-24 13:28:55 +0530

    Page for admin to approve student done

---
+++ Pages/Students/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SjcVotersPortal.Data;
+
+namespace SjcVotersPortal.Pages.Students;
+
+public class Index : SiteadminBasePageModel
+{
+    private readonly ApplicationDbContext _context;
+
+    public Index(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IList<Student> Students { get;set; } = default!;
+
+    public async Task OnGet()
+    {
+        Students = await _context.Students.ToListAsync();
+    }
+}
