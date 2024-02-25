0df7621e129b48f4ea14707f5d0705a5d82b2004
Sheik Rasheed <sheikrasheed@gmail.com>
2024-02-25 18:28:59 +0530

    View Nomination feature was added

--- Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -16,6 +16,7 @@ using Microsoft.AspNetCore.WebUtilities;
 
 namespace SjcVotersPortal.Areas.Identity.Pages.Account
 {
+    [AllowAnonymous]
     public class ForgotPasswordModel : PageModel
     {
         private readonly UserManager<IdentityUser> _userManager;
---
+++ Pages/ViewNominations.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SjcVotersPortal.Data;
+using SjcVotersPortal.Data.Models;
+
+namespace SjcVotersPortal.Pages;
+
+public class ViewNominations : SiteadminBasePageModel
+{
+    private readonly ApplicationDbContext _context;
+
+    public ViewNominations(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    public Dictionary<(int ElectionId, string AssociationName), Dictionary<(int DesignationId, string DesignantionName), List<Student>>> Data;
+    
+    public void OnGet()
+    {
+        var now = DateTime.Now;
+        var currentElections = _context.Elections.Include(e => e.Association).Where(e => now > e.NominationStart /*&& e.VotingEnd > now*/);
+        Data = currentElections.ToDictionary(election => (election.Id, election.Association.Name),
+            election => _context.AssociationDesignations.Where(associationDesignation => associationDesignation.AssociationId == election.AssociationId)
+                .Select(associationDesignation => associationDesignation.Designation).ToDictionary(designation => (designation.Id, designation.Name),
+                    _ => new List<Student>()));
+    
+        foreach (var item in Data)
+        {
+            foreach (var item2 in item.Value)
+            {
+                var students = _context.Nominations.Where(e => e.ElectionId == item.Key.ElectionId && e.DesignationId == item2.Key.DesignationId).Select(e => e.Student);
+                item2.Value.AddRange(students);
+            }
+        }
+    }
+}
